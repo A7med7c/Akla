b8b013ef9214d0cb6ccd7e92a8fa502d43a52d1a
Ahmed Ragab <ragabali@std.mans.edu.eg>
2026-06-27 22:49:33 +0300

    Add Unit Test for CreateRestaurantCommand

---
+++ Restaurants.Domain.Test/CreateRestaurantCommandValidatorTest.cs
@@ -0,0 +1,53 @@
+﻿using FluentValidation.TestHelper;
+using Restaurants.Application.Restaurants.Commands.CreateRestaurant;
+
+namespace Restaurants.Domain.Test
+{
+    public class CreateRestaurantCommandValidatorTest
+    {
+        [Fact]
+        public void Validator_ForValidCommand_ShoulldNotHaveValidationErrors()
+        {
+            // arrange
+            var command = new CreateRestaurantCommand()
+            {
+                Name = "test",
+                Category = "Italian",
+                ContactEmail = "test@test.com",
+                PostalCode = "12-345",
+                Description = "New Description for a restaurant"
+            };
+            var validator = new CreateRestaurantCommandValidator();
+
+            //act
+            var result = validator.TestValidate(command);
+
+            //assert 
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+
+        [Fact]
+        public void Validator_ForInValidCommand_ShoulldHaveValidationErrors()
+        {
+            // arrange
+            var command = new CreateRestaurantCommand()
+            {
+                Name = "te",
+                Category = "Masri",
+                ContactEmail = "@test.com",
+                PostalCode = "12-345",
+                Description = "New Description for a restaurant"
+            };
+            var validator = new CreateRestaurantCommandValidator();
+
+            //act
+            var result = validator.TestValidate(command);
+
+            //assert 
+            result.ShouldHaveValidationErrorFor(c => c.Name);
+            result.ShouldHaveValidationErrorFor(c => c.Category);
+            result.ShouldHaveValidationErrorFor(c => c.ContactEmail);
+        }
+    }
+}

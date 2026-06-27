98761a300eef7288f92b5f58762f0b9dc6f60501
Ahmed Ragab <ragabali@std.mans.edu.eg>
2026-06-27 20:40:39 +0300

    Add Unit Test For USer Roles

---
+++ Restaurants.Domain.Test/UnitTest1.cs
@@ -0,0 +1,52 @@
+using Restaurants.Application.Users;
+using Restaurants.Domain.Constants;
+
+namespace Restaurants.Domain.Test
+{
+    public class Usertest
+    {
+        [Fact]
+        public void IsInRole_WithMatchingRole_ShouldReturnTrue()
+        {
+            var user = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User, UserRoles.Owner, UserRoles.Driver], null, null);
+
+            var result = user.IsInRole(UserRoles.Admin);
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsInRole_WithNoMatchingRole_ShouldReturnFalse()
+        {
+            var user = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User, UserRoles.Owner], null, null);
+
+            var result = user.IsInRole(UserRoles.Driver);
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsInRole_WithNoMatchingRoleCase_ShouldReturnFalse()
+        {
+            var user = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.User, UserRoles.Owner], null, null);
+
+            var result = user.IsInRole(UserRoles.Admin.ToLower());
+
+            Assert.False(result);
+        }
+
+
+        [Theory]
+        [InlineData(UserRoles.Admin)]
+        [InlineData(UserRoles.Owner)]
+        public void IsInRole_WithMatchingRoles_ShouldReturnTrue(string roleName)
+        {
+            var user = new CurrentUser("1", "test@test.com", [UserRoles.Admin, UserRoles.Owner], null, null);
+
+            var result = user.IsInRole(roleName);
+
+            Assert.True(result);
+        }
+
+    }
+}
---
+++ Restaurants.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Restaurants.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
